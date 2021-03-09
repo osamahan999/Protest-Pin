abf323524241fc947880e37df11f6eeab915ef34
Pak Shing Kan <kennethkankps@gmail.com>
2021-03-09 13:32:59 -0800

    added map style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
+import mapStyles from './mapStyles'
+
 const containerStyle = {
   width: '100vw',
   height: '100vh'
@@ -11,22 +13,34 @@ const center = {
   lng: -121.893028,
 };
 
+const options ={
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl : true,
+}
+
 function App() {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 
   return (
-    <LoadScript
-      googleMapsApiKey = {apiKey}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+    <div>
+      <LoadScript
+        googleMapsApiKey = {apiKey}
       >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
+      <h1>
+        Protest Pin{" "}
+      </h1>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+          options={options}
+        >
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </div>
   )
 }
 
