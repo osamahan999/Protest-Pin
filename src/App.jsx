655b6a3a33019381efbc8a92c25f913cec0035b6
Pak Shing Kan <kennethkankps@gmail.com>
2021-03-09 14:02:25 -0800

    added new svg marker and onclick event

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React,{useState} from 'react'
+import { GoogleMap, LoadScript,Marker, } from '@react-google-maps/api';
 
 import mapStyles from './mapStyles'
 
@@ -21,6 +21,7 @@ const options ={
 
 function App() {
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  const [markers, setMarkers] = useState([])
 
   return (
     <div>
@@ -35,9 +36,22 @@ function App() {
           center={center}
           zoom={10}
           options={options}
+          onClick={(event)=>{
+            setMarkers(current => [...current,{
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng(),
+              time: new Date(),
+            }])
+          }}
         >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
+         {markers.map(marker => <Marker key={marker.time.toISOString()} 
+         position={{lat:marker.lat, lng: marker.lng}}
+         icon={{
+           url: `/loudspeaker.svg`,
+           scaledSize: new window.google.maps.Size(30,30)
+         }}
+         >
+         </Marker>)}
         </GoogleMap>
       </LoadScript>
     </div>
