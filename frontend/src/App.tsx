4938b2d1fab761942647fb102c5d680c3e26c26b
Julie Jacobson <juliejacobson@Julies-MacBook-Pro-2.local>
2021-03-13 21:14:33 -0800

    added welcome, login, sign up modals

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { WelcomeModal } from './WelcomeModal';
+import Modal from './Modal';
+import { HomePage } from './HomePage'
 
 function App() {
+  // const openWelcome = () => {
+  //   setWelcome(prev => !prev);
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {/* <WelcomeModal showWelcome={showWelcome} setWelcome={setWelcome} />     */}
+        <HomePage />  
+      </div>
+    </>
+    
   );
 }
 
