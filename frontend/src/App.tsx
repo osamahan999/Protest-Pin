9b84691ee9e53c048b11606d0524cacdc0019080
Julie Jacobson <julsjacobson@gmail.com>
2021-03-13 21:23:56 -0800

    merging app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,15 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { WelcomeModal } from './WelcomeModal';
-import Modal from './Modal';
 import { HomePage } from './HomePage'
 
 function App() {
-  // const openWelcome = () => {
-  //   setWelcome(prev => !prev);
-  // }
   return (
-    <>
-      <div className="App">
-        {/* <WelcomeModal showWelcome={showWelcome} setWelcome={setWelcome} />     */}
+      <div>
         <HomePage />  
       </div>
-    </>
     
   );
 }
 
-export default App;
+export default React.memo(App);
