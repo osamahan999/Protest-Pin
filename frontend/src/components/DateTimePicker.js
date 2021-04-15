e9486b7fb45c567ee96b458ab87935e7ae980f1e
Pak Shing Kan <kennethkankps@gmail.com>
2021-04-15 14:59:50 -0700

    change to a new date time picker for create event

---
+++ frontend/src/components/DateTimePicker.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+}));
+
+export default function DateTimePicker({setVisitDate}) {
+    const classes = useStyles();
+    const [dateObj,setDateObj] = useState() 
+
+    const onChange = (e) =>{
+       
+        setVisitDate(e.target.value)
+
+    }
+  
+
+  return (
+    <form className={classes.container} noValidate>
+      <TextField
+        id="datetime-local"
+        label="Next appointment"
+        type="datetime-local"
+        defaultValue="2017-05-24T10:30"
+        onChange = {e=>onChange(e)}
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+    </form>
+  );
+}
