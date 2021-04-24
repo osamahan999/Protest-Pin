f0e7068e054d9c91391de58ccae90818ba967f7f
Pak Shing Kan <kennethkankps@gmail.com>
2021-04-24 12:01:31 -0700

    added rating on infoModal

--- frontend/src/components/InfoModal.jsx
+++ frontend/src/components/InfoModal.jsx
@@ -8,6 +8,7 @@ import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import CancelIcon from '@material-ui/icons/Cancel';
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
+import CustomizedRatings from './CustomizedRatings'
 
 
 
@@ -32,6 +33,7 @@ export default function InfoModal({latitude, longitude, event_name,time_of_event
             <h4 className="card-title">{event_name}</h4>
             <p className="description"> {event_description}</p>
             <p className="card-text text-secondary"> {time_of_event}</p>
+            <CustomizedRatings></CustomizedRatings>
         </div>
         <ButtonGroup
             fullWidth={true}
